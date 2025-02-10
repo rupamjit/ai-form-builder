b9d9e7eb8ab7604a42fdbe8d5d8f3ee44f1c40d0
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-02-10 16:18:26 +0530

    setup clerk authentication

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <p className='font-extrabold text-2xl'>
+            Sorry !!! Page Not Found
+        </p>
+    </div>
+  )
+}
+
+export default NotFound
