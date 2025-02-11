1e9faa0ba8dc35e985f3c80503ff3f3dc110c466
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-02-11 15:29:24 +0530

    handle getAllForms

---
+++ actions/getAllForms.ts
@@ -0,0 +1,34 @@
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+export const getForms = async () => {
+  try {
+    const user = await currentUser();
+
+    if (!user) {
+      return { success: false, message: "User Not Found" };
+    }
+
+    const forms = await prisma.form.findMany({
+      where: {
+        ownerId: user.id,
+      },
+    });
+
+    if (!forms) {
+      return { success: false, message: "Forms Not Found" };
+    }
+
+    return {
+      success: true,
+      message: "Form generated successfully.",
+      data: forms,
+    };
+  } catch (error) {
+    console.log("Error generating form:", error);
+    return {
+      success: false,
+      message: "An error occurred while generating the form",
+    };
+  }
+};
